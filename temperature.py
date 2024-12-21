ae29dc2699aa005975bdf7f15f4ed635f230ae57
Yadshiro <yasel.rubioribeaux@web.de>
2024-12-21 14:05:21 +0100

    Cambios

--- temperature.py
+++ temperature.py
@@ -1,9 +1,9 @@
-temperature =int(input( "Que temperatura hay en la Habana?\n"))
+temperature =int(input( "Que temperatura hay en la Habana en grados:?\n"))
 
 if temperature > 25:
     print("Eso es muy caliente debo de quiedarme en casa!!!!")
-if temperature < 25:
+elif temperature <= 25:
     print("Eso es agradable puedo salir todo el dia !!!!!")
-if temperature == 25:
-    print("Eso es normal puedo salir en la tarde")
+
+
 
